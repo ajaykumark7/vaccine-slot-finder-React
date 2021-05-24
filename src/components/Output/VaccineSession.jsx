8977a4b512f1a77f74572ed2da2171e9254daa76
h20170039 <46812748+h20170039@users.noreply.github.com>
2021-05-24 18:47:01 +0530

    Minor changes

--- src/components/Output/VaccineSession.jsx
+++ src/components/Output/VaccineSession.jsx
@@ -1,5 +1,5 @@
 function VaccineSession(props) {
-    console.log(props)
+    console.log("INSIDE VACCINE SESSION COMPONENT")
     return (
         <div className='vaccination-session'>{props['centerDetails']['name']} {props['sessionDetails']['date']} {props['sessionDetails']['min_age_limit']} {props['sessionDetails']['available_capacity']}</div>
     );
